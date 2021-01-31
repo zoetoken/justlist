a9fc93178c2c02421e3620d6ed6ac086e73b3122
zoetoken <78313299+zoetoken@users.noreply.github.com>
2021-01-31 12:48:46 -0500

    Update and rename justlist.ts to zoeList.ts

---
+++ zoeList.ts
@@ -0,0 +1,19 @@
+{
+   "name": "zoeList", #
+   "logoURI": "https://coin.top/production/logo/usdj.png", #List logo uri
+   "timestamp": 1610339529000, #Update time in millisecond
+   "tokens": [{
+           "chainId": 1, #ChainId, which is currently 1
+           "address": "TGjtuzFYo5jZvRE7ZEN3zP5XoxxU26CLT5", #Token address
+           "name": "ZOE TOKEN", #Token name
+           "symbol": "ZOE", #Token symbol
+           "decimals": 6, #Token decimals
+           "logoURI": "https://zoetoken.org/images/logo.png" #Token logo uri
+       }
+   ],
+   "version": { 
+       "major": 1, #Major version
+       "minor": 0, #Minor version
+       "patch": 0  #Patch version
+   }
+}
